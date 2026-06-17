286f5a418f9d3f7af1249833ce9421fb444e8064
tais <tais.sferreira5@senacsp.edu.br>
2026-06-17 14:34:30 -0300

    criação dos arquivos cookies, politica de privacidade e segurança privacidade

--- database/migrations/0001_01_01_000000_create_usuarios_table.php
+++ database/migrations/0001_01_01_000000_create_usuarios_table.php
@@ -11,9 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('usuarios', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 100);
+            $table->string('nome', 100);
             $table->string('email', 100)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
@@ -51,7 +51,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('usarios');
         Schema::dropIfExists('password_reset_tokens');
         Schema::dropIfExists('sessions');
     }
---
+++ resources/views/politica-privacidade.blade.php
@@ -0,0 +1,141 @@
+@extends('layouts.site')
+
+@section('conteudo')
+
+<div id="menusPolitica">
+        <ul>
+          <li>
+            <a href="politica-privacidade.html">Política de Privacidade</a>
+          </li>
+          <li>
+            <a href="cookies.html">Política de Cookies</a>
+          </li>
+          <li>
+            <a href="termos-uso.html">Termos de Uso</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="politica">
+        <h1>POLÍTICA DE PRIVACIDADE</h1>
+        <p id="dataPublicacao">Publicado em: 14/11/2025</p>
+        <br /><br />
+        <hr />
+        <br /><br />
+        <p id="politicaConteudo">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis
+          massa tempus, porttitor odio et, eleifend neque. Quisque convallis
+          ipsum id nunc accumsan porta. Nam mattis euismod eros ut pellentesque.
+          Integer sodales enim a est mollis, quis dictum libero porta.
+          Pellentesque feugiat nec dui ut sollicitudin. Donec placerat nisi
+          vitae augue varius vestibulum in sit amet libero. Aliquam condimentum
+          nisl nec massa eleifend imperdiet.<br /><br />
+
+          In nec erat vel tellus accumsan rhoncus non eu ipsum. Donec tempus,
+          quam vel aliquet vestibulum, erat augue cursus metus, et venenatis
+          nulla ante non est. Vestibulum nisl sapien, sollicitudin in ultricies
+          nec, luctus sit amet sapien. Fusce diam ex, viverra sed tristique
+          vitae, vestibulum eu eros. Pellentesque habitant morbi tristique
+          senectus et netus et malesuada fames ac turpis egestas. Cras a tempor
+          nulla. Morbi lacinia dui nisi, at suscipit massa ornare eu. Nullam
+          vulputate vehicula magna nec interdum. Nam venenatis ligula at odio
+          dictum, nec congue sapien vulputate. Cras aliquam erat velit, sed
+          lacinia nunc pellentesque vitae. Vestibulum facilisis dolor nulla, a
+          dignissim leo pulvinar in.<br /><br />
+
+          Maecenas mattis scelerisque nisi, et vestibulum ante aliquam non.
+          Curabitur libero sem, suscipit sit amet velit a, ornare ornare orci.
+          Phasellus ultrices porttitor dui, eu porta nisl dapibus et. Phasellus
+          eget elementum nunc, quis commodo nisl. In hac habitasse platea
+          dictumst. Duis pellentesque erat enim, at pulvinar lorem viverra vel.
+          Duis vel congue magna, vel vestibulum neque. Proin eu auctor orci. Ut
+          quis tristique sapien, eget finibus sapien. Sed ultricies quam eget
+          leo auctor scelerisque. Nullam tellus nisi, dictum sit amet ultrices
+          a, rhoncus a nisl. Phasellus at quam sed sem varius fringilla vel non
+          leo. Suspendisse vel accumsan turpis. Nulla feugiat fermentum odio.
+          Donec congue eu turpis non vehicula.<br /><br />
+
+          Praesent augue diam, vehicula a feugiat tempus, fermentum non leo. Ut
+          volutpat, turpis a ullamcorper dapibus, magna quam vulputate libero,
+          sit amet maximus ex neque vel ipsum. Sed vitae gravida elit. Nunc
+          facilisis tristique magna a dictum. Praesent vitae nisi in dui semper
+          pellentesque id sed velit. Morbi faucibus fringilla libero, sed rutrum
+          velit maximus eu. Etiam eget viverra lectus. Pellentesque ut arcu in
+          sem congue consequat vel eget erat. Aliquam diam orci, pretium sit
+          amet dignissim quis, iaculis et magna. Etiam consequat neque id lacus
+          rutrum fringilla. Integer sem ex, efficitur a vulputate at, aliquam
+          eget lorem. Aliquam porttitor diam nulla, in lobortis ligula
+          vestibulum et. Suspendisse lobortis justo sit amet malesuada
+          faucibus.<br /><br />
+
+          Morbi hendrerit tristique vehicula. Vestibulum pellentesque, massa eu
+          varius hendrerit, dui enim condimentum orci, ut vulputate purus turpis
+          egestas sapien. Fusce porta, purus faucibus vestibulum hendrerit,
+          lectus libero mattis tortor, ut interdum diam mauris vel magna.
+          Maecenas risus elit, mattis ut aliquam et, mollis quis quam. Maecenas
+          ipsum urna, ornare ut iaculis sed, feugiat in libero. In et arcu id
+          tellus mattis pulvinar. In rutrum cursus elit, id commodo quam. Nullam
+          non consectetur ante, nec posuere justo. Maecenas eu mauris interdum,
+          placerat metus in, gravida leo. Phasellus tortor velit, molestie
+          ornare pellentesque ac, vulputate vitae nisi. Donec iaculis pharetra
+          arcu. Aenean volutpat ultricies porta. Duis vitae tempus turpis.<br /><br />
+
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis
+          massa tempus, porttitor odio et, eleifend neque. Quisque convallis
+          ipsum id nunc accumsan porta. Nam mattis euismod eros ut pellentesque.
+          Integer sodales enim a est mollis, quis dictum libero porta.
+          Pellentesque feugiat nec dui ut sollicitudin. Donec placerat nisi
+          vitae augue varius vestibulum in sit amet libero. Aliquam condimentum
+          nisl nec massa eleifend imperdiet.<br /><br />
+
+          In nec erat vel tellus accumsan rhoncus non eu ipsum. Donec tempus,
+          quam vel aliquet vestibulum, erat augue cursus metus, et venenatis
+          nulla ante non est. Vestibulum nisl sapien, sollicitudin in ultricies
+          nec, luctus sit amet sapien. Fusce diam ex, viverra sed tristique
+          vitae, vestibulum eu eros. Pellentesque habitant morbi tristique
+          senectus et netus et malesuada fames ac turpis egestas. Cras a tempor
+          nulla. Morbi lacinia dui nisi, at suscipit massa ornare eu. Nullam
+          vulputate vehicula magna nec interdum. Nam venenatis ligula at odio
+          dictum, nec congue sapien vulputate. Cras aliquam erat velit, sed
+          lacinia nunc pellentesque vitae. Vestibulum facilisis dolor nulla, a
+          dignissim leo pulvinar in.<br /><br />
+
+          Maecenas mattis scelerisque nisi, et vestibulum ante aliquam non.
+          Curabitur libero sem, suscipit sit amet velit a, ornare ornare orci.
+          Phasellus ultrices porttitor dui, eu porta nisl dapibus et. Phasellus
+          eget elementum nunc, quis commodo nisl. In hac habitasse platea
+          dictumst. Duis pellentesque erat enim, at pulvinar lorem viverra vel.
+          Duis vel congue magna, vel vestibulum neque. Proin eu auctor orci. Ut
+          quis tristique sapien, eget finibus sapien. Sed ultricies quam eget
+          leo auctor scelerisque. Nullam tellus nisi, dictum sit amet ultrices
+          a, rhoncus a nisl. Phasellus at quam sed sem varius fringilla vel non
+          leo. Suspendisse vel accumsan turpis. Nulla feugiat fermentum odio.
+          Donec congue eu turpis non vehicula.<br /><br />
+
+          Praesent augue diam, vehicula a feugiat tempus, fermentum non leo. Ut
+          volutpat, turpis a ullamcorper dapibus, magna quam vulputate libero,
+          sit amet maximus ex neque vel ipsum. Sed vitae gravida elit. Nunc
+          facilisis tristique magna a dictum. Praesent vitae nisi in dui semper
+          pellentesque id sed velit. Morbi faucibus fringilla libero, sed rutrum
+          velit maximus eu. Etiam eget viverra lectus. Pellentesque ut arcu in
+          sem congue consequat vel eget erat. Aliquam diam orci, pretium sit
+          amet dignissim quis, iaculis et magna. Etiam consequat neque id lacus
+          rutrum fringilla. Integer sem ex, efficitur a vulputate at, aliquam
+          eget lorem. Aliquam porttitor diam nulla, in lobortis ligula
+          vestibulum et. Suspendisse lobortis justo sit amet malesuada
+          faucibus.<br /><br />
+
+          Morbi hendrerit tristique vehicula. Vestibulum pellentesque, massa eu
+          varius hendrerit, dui enim condimentum orci, ut vulputate purus turpis
+          egestas sapien. Fusce porta, purus faucibus vestibulum hendrerit,
+          lectus libero mattis tortor, ut interdum diam mauris vel magna.
+          Maecenas risus elit, mattis ut aliquam et, mollis quis quam. Maecenas
+          ipsum urna, ornare ut iaculis sed, feugiat in libero. In et arcu id
+          tellus mattis pulvinar. In rutrum cursus elit, id commodo quam. Nullam
+          non consectetur ante, nec posuere justo. Maecenas eu mauris interdum,
+          placerat metus in, gravida leo. Phasellus tortor velit, molestie
+          ornare pellentesque ac, vulputate vitae nisi. Donec iaculis pharetra
+          arcu. Aenean volutpat ultricies porta. Duis vitae tempus turpis.
+        </p>
+      </div>
+@endsection
