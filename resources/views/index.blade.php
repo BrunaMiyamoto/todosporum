ea8626b0b9bbb5011453c56fce4e0ef5c2779866
Fernanda Oliveira <fernandaaoliveira7002@gmail.com>
2026-07-06 16:14:59 -0300

    index css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,86 +1,94 @@
 @extends('layouts.site')
 
 @section('conteudo')
-    <div id="banner">
-        <h1 id="tituloBanner">ENVIE SUA IDEIA JÁ</h1>
-        <p id="textoBanner">
-            Participe ativamente do debate público: envie projetos, proponha
-            pautas, registre reclamações e interaja com outros cidadãos. <br />
-            Vote nas melhores iniciativas e ajude a transformar sua comunidade.
-            Exerça seu papel de cidadão e faça a diferença!
-        </p>
-        <a href="login.html" id="botaoEscreva">Escreva aqui...</a>
-        <br />
-        <a href="forum-projetos.html" id="botaoExplore">Explore o Fórum</a>
+    <div class="relative bg-cover bg-no-repeat bg-center min-h-64 "
+        style="background-image: url('{{ asset('assets/img/pexels-cytonn-955389.jpg') }}')">
+        <div class="absolute inset-0 bg-gradient-to-r from-sky-800 to-slate-400 mix-blend-multiply"></div>
+
+        <div class="relative z-10 flex flex-col justify-center items-center py-12">
+            <h1 class="text-white font-semibold text-center">ENVIE SUA IDEIA JÁ</h1>
+            <p class="text-white text-center p-6">
+                Participe ativamente do debate público: envie projetos, proponha
+                pautas, registre reclamações e interaja com outros cidadãos. <br />
+                Vote nas melhores iniciativas e ajude a transformar sua comunidade.
+                Exerça seu papel de cidadão e faça a diferença!
+            </p>
+            <a href="login.html" class="text-[#e9702a] block text-center m-auto max-w-3xl bg-white p-3 rounded-xl">Escreva
+                aqui...</a>
+            <br />
+            <a href="forum-projetos.html" class="block text-center text-[#99cef3]">Explore o Fórum</a>
+        </div>
     </div>
 
-    <h2 class="text-[#05668d] text-center mt-20 mb-8 font-bold text-3xl ">DESTAQUES DA COMUNIDADE</h2>
+    <h2 class="text-[#05668d] text-center mt-20 font-bold text-3xl ">DESTAQUES DA COMUNIDADE</h2>
 
-    <div class=" flex-1">
+    <div class=" flex min-h-screen w-full items-center justify-center">
+        <div class="flex flex-col ">
 
-        <section class="destaque ">
-            <div class="cabeca">
-                <div class="tagsDestaque">
-                    <ul class="destaqueUl">
-                        <li class="destaqueLi">Ensino</li>
-                    </ul>
+            <section class="destaque ">
+                <div class="cabeca">
+                    <div class="tagsDestaque">
+                        <ul class="destaqueUl">
+                            <li class="destaqueLi">Ensino</li>
+                        </ul>
+                    </div>
+                    <div class="usuarioDestaque">Projeto por: Usuário00002</div>
                 </div>
-                <div class="usuarioDestaque">Projeto por: Usuário00002</div>
-            </div>
-            <h3 class="linhalaranja">
-                Projeto “Educação Política nas Escolas” - Formação de Jovens Cidadãos
-            </h3>
-            <p>
-                <strong>Objetivo:</strong> Implementar oficinas e palestras sobre
-                cidadania, política, direitos e deveres nas escolas públicas de
-                Marília, promovendo o engajamento político desde a juventude.
-            </p>
-        </section>
+                <h3 class="linhalaranja">
+                    Projeto “Educação Política nas Escolas” - Formação de Jovens Cidadãos
+                </h3>
+                <p>
+                    <strong>Objetivo:</strong> Implementar oficinas e palestras sobre
+                    cidadania, política, direitos e deveres nas escolas públicas de
+                    Marília, promovendo o engajamento político desde a juventude.
+                </p>
+            </section>
 
 
-        <section class="destaque">
-            <div class="cabeca">
-                <div class="tagsDestaque">
-                    <ul class="destaqueUl">
-                        <li class="destaqueLi">Infraestrutura</li>
-                        <li class="destaqueLi">Natureza</li>
-                    </ul>
+            <section class="destaque">
+                <div class="cabeca">
+                    <div class="tagsDestaque">
+                        <ul class="destaqueUl">
+                            <li class="destaqueLi">Infraestrutura</li>
+                            <li class="destaqueLi">Natureza</li>
+                        </ul>
+                    </div>
+                    <div class="usuarioDestaque">Projeto por: Usuário00003</div>
                 </div>
-                <div class="usuarioDestaque">Projeto por: Usuário00003</div>
-            </div>
-            <h3 class="linhalaranja">Projeto “Praça Viva” - Revitalização de Espaços Públicos</h3>
-            <p>
-                <strong>Objetivo:</strong> Transformar praças abandonadas ou pouco
-                utilizadas em espaços comunitários ativos, com infraestrutura básica
-                (bancos, iluminação, lixeiras), área verde, brinquedos para crianças e
-                espaços para atividades culturais e esportivas.
-            </p>
-        </section>
-        <section class="destaque">
-            <div class="cabeca">
-                <div class="tagsDestaque">
-                    <ul class="destaqueUl">
-                        <li class="destaqueLi">Comunicação</li>
-                        <li class="destaqueLi">Tecnologia</li>
-                    </ul>
+                <h3 class="linhalaranja">Projeto “Praça Viva” - Revitalização de Espaços Públicos</h3>
+                <p>
+                    <strong>Objetivo:</strong> Transformar praças abandonadas ou pouco
+                    utilizadas em espaços comunitários ativos, com infraestrutura básica
+                    (bancos, iluminação, lixeiras), área verde, brinquedos para crianças e
+                    espaços para atividades culturais e esportivas.
+                </p>
+            </section>
+            <section class="destaque">
+                <div class="cabeca">
+                    <div class="tagsDestaque">
+                        <ul class="destaqueUl">
+                            <li class="destaqueLi">Comunicação</li>
+                            <li class="destaqueLi">Tecnologia</li>
+                        </ul>
+                    </div>
+                    <div class="usuarioDestaque">Projeto por: Todos por Um</div>
                 </div>
-                <div class="usuarioDestaque">Projeto por: Todos por Um</div>
-            </div>
-            <h3 class="linhalaranja">
-                Projeto “Ouvidoria Cidadã Digital” - Plataforma de Escuta Popular
-            </h3>
-            <p>
-                <strong>Objetivo:</strong> Criar um canal digital oficial de escuta
-                permanente da população, onde qualquer cidadão possa registrar
-                sugestões, reclamações ou ideias diretamente vinculadas às secretarias
-                municipais e vereadores.
-            </p>
-        </section>
+                <h3 class="linhalaranja">
+                    Projeto “Ouvidoria Cidadã Digital” - Plataforma de Escuta Popular
+                </h3>
+                <p>
+                    <strong>Objetivo:</strong> Criar um canal digital oficial de escuta
+                    permanente da população, onde qualquer cidadão possa registrar
+                    sugestões, reclamações ou ideias diretamente vinculadas às secretarias
+                    municipais e vereadores.
+                </p>
+            </section>
+        </div>
     </div>
 
     <section class="entendarapido">
-        <h2>ENTENDA RÁPIDO</h2>
-        <div id="videocentro">
+        <h2 class="text-[#05668d] text-center mb-8 font-bold text-3xl ">ENTENDA RÁPIDO</h2>
+        <div class="text-center">
             <video class="video" poster="assets/img/capa2.png" controls>
                 <source src="assets/img/gato-teste.mp4" type="video/mp4" />
             </video>
@@ -92,13 +100,14 @@
             </video>
         </div>
         <br />
-        <a href="aprenda-videos.html" id="entendabotao">Veja mais vídeos explicativos</a>
+        <a href="aprenda-videos.html" class="bg-[#629643] text-white py-1 px-2 rounded-md block text-center m-8 w-64">Veja
+            mais vídeos explicativos</a>
     </section>
 
-    <section class="sobrenos">
-        <div id="textosobrenos">
-            <h2>CONHEÇA MAIS SOBRE NÓS</h2>
-            <p>
+    <section class="flex flex-row items-center justify-center gap-10 py-14 px-5 max-w-6xl m-8 box-border">
+        <div class="flex-1 min-w-auto text-justify justify-center ">
+            <h2 class="text-[#05668d] mb-2 font-bold text-3xl ">CONHEÇA MAIS SOBRE NÓS</h2>
+            <p class="paragrafo">
                 O projeto “Todos Por Um” consiste em uma plataforma digital
                 independente e sem fins lucrativos, destinada à população de
                 Marília. Por meio do nosso website, os cidadãos poderão propor
@@ -107,7 +116,7 @@
                 encaminhar essas propostas à Câmara Municipal para apreciação e
                 eventual votação.
             </p>
-            <p>
+            <p class="paragrafo">
                 Nossa iniciativa busca combater a baixa participação política da
                 população, promovendo maior engajamento cívico e fortalecendo o
                 vínculo entre os moradores e as decisões tomadas no âmbito
@@ -115,14 +124,14 @@
                 ao conhecimento político, estimular o debate público e contribuir
                 para o desenvolvimento coletivo da cidade.
             </p>
-            <p>
+            <p class="paragrafo">
                 Por meio do “Todos Por Um”, acreditamos que é possível construir uma
                 Marília mais participativa, transparente e inclusiva.
             </p>
             <a href="sobre-nos.html" class="sobrenosbotao">Saiba mais sobre Todos Por Um</a>
             <a href="contato.html" class="sobrenosbotao">Entre em contato conosco</a>
         </div>
-        <div id="imagemsobrenos">
+        <div class="rounded-2xl max-w-full h-auto">
             <img src="assets/img/pexels-polina-tankilevitch-8203158-scaled.jpg" alt="Mulher gritando em um megafone"
                 width="600" height="400" />
         </div>
