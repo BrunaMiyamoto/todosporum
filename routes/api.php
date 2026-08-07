69b664e38d0718e2a6f02a29764b6154141a4696
bruna miyamoto <brumiyamoto00@gmail.com>
2026-08-07 17:07:10 -0300

    instalação Api postagens

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\Api\AuthApiController;
+use App\Http\Controllers\Api\PostagemApiController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+
+Route::apiResource('postagens', PostagemApiController::class);
+Route::post('/login',[AuthApiController::class, "login"])->middleware("auth:sanctum");
